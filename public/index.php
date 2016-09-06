2dc0c65a1329142970d95e078dbbc6a89aa72d66
nick032 <nick.x.a.k@gmail.com>
2016-09-06 16:23:32 +0300

    урок 2
    улучшения класса Router

--- public/index.php
+++ public/index.php
@@ -1,14 +1,29 @@
 <?php
 $query = $_SERVER['QUERY_STRING'];
+
+define('WWW', __DIR__);
+define('CORE', dirname(__DIR__) . '/vendor/core');
+define('APP', dirname(__DIR__) . '/app');
+define('ROOT', dirname(__DIR__));
+
 require_once '../vendor/core/Router.php';
 require_once '../vendor/libs/functions.php';
+// require_once '../app/controllers/Main.php';
+// require_once '../app/controllers/Posts.php';
+// require_once '../app/controllers/PostsNew.php';
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts/', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+spl_autoload_register(function($class){
+	$file = APP . "/controllers/$class.php";
+	if(is_file($file)) {
+		require_once($file);
+	}
+});
 
-if(Router::matchRoute($query)){
-    debug(Router::getRoute());
-}else{
-    echo('404');
-}
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Posts']);
+
+// Defaults routes
+
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
+
+Router::dispatch($query);
